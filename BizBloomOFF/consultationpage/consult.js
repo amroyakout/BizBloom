c7923b9486baf25d58a0dd7b46a80eecd2e391bd
trickeytrick <147985142+trickeytrick@users.noreply.github.com>
2026-03-31 21:03:06 +0100

    Consult -> email system complete
    Now completed the consult system fully working. Removed indiv and org consult and combined them into one

---
+++ BizBloomOFF/consultationpage/consult.js
@@ -0,0 +1,26 @@
+const preSubmitUi = document.getElementById("preSubmit");
+const form = document.getElementById("detailsForm");
+
+(function(){
+  emailjs.init("h84JytWxmC4Z_MG-c");
+})();
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    emailjs.send("service_aj51c8h", "template_2rcec47", {
+        name: document.getElementById("nameInput").value,
+        email: document.getElementById("emailInput").value,
+        phone: document.getElementById("phoneInput").value,
+        date: document.getElementById("preferredDate").value,
+        time: document.getElementById("preferredTime").value,
+        brief: document.getElementById("briefInput").value
+    })
+    .then(function() {
+        alert("Sent consultation email to consulter(s)!");
+    })
+    .catch(function(error) {
+        alert("Failed to send!")
+        console.log(error);
+    });
+});
